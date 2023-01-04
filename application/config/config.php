3d3d7cf7488703cddad53ed324c862b460ff173f
charlesbintang <charlesbintang203@gmail.com>
2023-01-04 10:19:40 +0700

    Ganti base URl

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/PBLS3Ikhsan/';
+$config['base_url'] = 'http://localhost/PBL_MJ009/';
 
 /*
 |--------------------------------------------------------------------------
